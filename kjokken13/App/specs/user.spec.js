d68d6b64e9c0c928c9899e1cc37a78b17b4ad55e
Lars Erik Finholt <lars.erik.finholt@gmail.com>
2013-08-18 13:19:39 +0200

    specs

---
+++ kjokken13/App/specs/user.spec.js
@@ -0,0 +1,11 @@
+﻿define(["viewmodels/user"], function (target) {
+    describe("User", function () {
+
+
+        it("should be possible to create instance", function () {
+            var a = new target.User({ user: 'user1', calendar: 'cal@cal.com' });
+            a.loadCalendar
+
+        });
+    });
+});
